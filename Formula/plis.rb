1aca7e04c678e2c1300fe62f0152ef70dbfec359
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-03-16 13:49:40 -0600

    Working plis Formula

--- Formula/plis.rb
+++ Formula/plis.rb
@@ -1,19 +1,20 @@
-require 'formula'
-
 class Plis < Formula
-  homepage "https://github.com/IcaliaLabs/plis"
-  head "https://github.com/IcaliaLabs/plis.git"
-  sha256 "acea8756c3baee82e197d3a6dce6c8b524d704054793773f0029b5afc076336f"
+  desc 'Common Docker/Compose development commands abbreviated by asking nicely'
+  homepage 'https://github.com/IcaliaLabs/plis'
+
   url 'https://github.com/IcaliaLabs/plis/releases/download/v0.0.0.build6/plis-darwin_x86_64'
   version '0.0.0.build6'
+  sha256 'acea8756c3baee82e197d3a6dce6c8b524d704054793773f0029b5afc076336f'
+
+  head 'https://github.com/IcaliaLabs/plis.git'
 
   def install
-    bin.install "bin/plis"
-  end
-
-  devel do
+    prefix.install "plis-darwin_x86_64"
+    mv prefix/"plis-darwin_x86_64", prefix/"plis"
+    bin.install prefix/"plis"
   end
 
   test do
+    system "#{bin}/plis", "--version"
   end
 end
