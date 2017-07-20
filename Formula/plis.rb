7766bc580f7e03bd7e3e28284bdedacdc986d808
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-07-20 18:51:31 -0500

    Updated plis to version 0.0.0.build7

--- Formula/plis.rb
+++ Formula/plis.rb
@@ -2,9 +2,9 @@ class Plis < Formula
   desc "Common Docker/Compose development commands abbreviated by asking nicely"
   homepage "https://github.com/IcaliaLabs/plis"
 
-  url "https://github.com/IcaliaLabs/plis/releases/download/v0.0.0.build6/plis-darwin_x86_64"
-  version "0.0.0.build6"
-  sha256 "acea8756c3baee82e197d3a6dce6c8b524d704054793773f0029b5afc076336f"
+  url "https://github.com/IcaliaLabs/plis/releases/download/v0.0.0.build7/plis-darwin_x86_64"
+  version "0.0.0.build7"
+  sha256 "0d95a568df3951d82f990c931c8a5cbaa9c76a6f23bb4e49e3f949d22b81b59f"
 
   head "https://github.com/IcaliaLabs/plis.git"
 
